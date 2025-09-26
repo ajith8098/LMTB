fddc12dda8f4e1155ad9b3b9837652743e8245f2
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-09-27 03:50:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("23990433")
+API_HASH = os.getenv("e6c4b6ee1933711bc4da9d7d17e1eb20")
+BOT_TOKEN = os.getenv("7972560151:AAHlUN7BOsfPwu-LFUpuAV0YLqyGdkEXsrU")
 
-MEGA_EMAIL = os.getenv("MEGA_EMAIL", "")
-MEGA_PASSWORD = os.getenv("MEGA_PASSWORD", "")
+MEGA_EMAIL = os.getenv("MEGA_EMAIL", "youtu0323@gmail.com")
+MEGA_PASSWORD = os.getenv("MEGA_PASSWORD", "12345678Sa@#$")
 
 DOWNLOAD_DIR = "/data/files"
